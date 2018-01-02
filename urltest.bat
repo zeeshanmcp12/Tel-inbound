6c66597da39b522b7d35c4dd6d620e622b917804
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2018-01-03 03:55:11 +0500

    Adding scripts to git.

---
+++ urltest.bat
@@ -0,0 +1,3 @@
+@echo off
+\\10.175.11.107\Pilotproject\build\exe.win32-3.6\urltest.exe %*
+@pause
---
+++ urltest.py
@@ -0,0 +1,23 @@
+import os, webbrowser
+#! python3
+
+
+##csp = 'http://10.2.55.43:8698/csp/bsf/index.action'
+seible = '//10.1.4.37/ecommunications_enu/start.swe'
+
+##icbs = 'https://10.218.14.168:7782'
+##icbsTest = 'http://10.218.14.168:7782/frameset/login.action'
+
+
+##  CSP
+webbrowser.open('https://10.2.55.43:8698/csp/bsf/index.action')
+
+##webbrowser.open_new_tab(csp)
+
+webbrowser.open(seible)
+
+##webbrowser.open(icbs)
+
+##  ICBS
+
+webbrowser.open('//10.218.14.168:7782/frameset/login.action')
