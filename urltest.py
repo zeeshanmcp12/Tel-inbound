6817fd3b536c90fc21b18b13899209325ca98c90
Muhammad Zeeshan <zeeshanmcp12@gmail.com>
2022-01-24 02:20:44 +0500

    Update urltest.py

--- urltest.py
+++ urltest.py
@@ -2,24 +2,24 @@ import os, webbrowser
 #! python3
 
 
-##csp = 'http://10.2.55.43:8698/csp/bsf/index.action'
-seible = '//10.1.4.37/ecommunications_enu/start.swe'
+##url = 'http://10.1.60.100:8000/index.action'
+url = '//10.1.60.100/ecommunications_enu/start.swe'
 
-##icbs = 'https://10.218.14.168:7782'
-##icbsTest = 'http://10.218.14.168:7782/frameset/login.action'
+##url = 'https://10.176.28.190:8277'
+##url = 'http://10.176.28.190:8277/frameset/login.action'
 
 
-##  CSP
-webbrowser.open('https://10.2.55.43:8698/csp/bsf/index.action')
+##  URL
+webbrowser.open('https://10.1.60.100:8000/index.action')
 
-##webbrowser.open_new_tab(csp)
+##webbrowser.open_new_tab(url)
 
-webbrowser.open(seible)
+webbrowser.open(url)
 
-##webbrowser.open(icbs)
+##webbrowser.open(url)
 
-##  ICBS
+##  URL
 
-webbrowser.open('//10.218.14.168:7782/frameset/login.action')
+webbrowser.open('//10.176.28.190:8277/login.action')
 
 webbrowser.open('https://www.youtube.com')
